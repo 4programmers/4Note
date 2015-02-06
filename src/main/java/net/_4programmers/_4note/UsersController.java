765cc8ea87c1f01724ef38e97ccda6729e01a14b
Andrzej Sołtysik <svizz17@gmail.com>
2015-02-06 12:11:31 +0100

    sample controller (just for dir structure)

---
+++ src/main/java/net/_4programmers/_4note/UsersController.java
@@ -0,0 +1,9 @@
+package net._4programmers._4note;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users/{userId}")
+public class UsersController {
+}
