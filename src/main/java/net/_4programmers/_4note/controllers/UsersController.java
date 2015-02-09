0b9aa58cf4df18524cf32b5e7d48548a3fa913de
Andrzej Sołtysik <svizz17@gmail.com>
2015-02-09 14:08:16 +0100

    No mapping found for [/]

--- src/main/java/net/_4programmers/_4note/controllers/UsersController.java
+++ src/main/java/net/_4programmers/_4note/controllers/UsersController.java
@@ -1,9 +1,32 @@
 package net._4programmers._4note.controllers;
 
+import net._4programmers._4note.dao.UserDao;
+import net._4programmers._4note.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
-@RequestMapping("/users/{userId}")
 public class UsersController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @RequestMapping("/user/{userId}")
+    public String getUserById(@PathVariable int userId, Model model){
+        model.addAttribute("user", userDao.getUserById(userId));
+        return "user";
+    }
+
+    @RequestMapping("/") public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/index") public String index2(){
+        return "index";
+    }
 }
---
+++ src/main/java/net/_4programmers/_4note/dao/impl/UserDaoImpl.java
@@ -0,0 +1,36 @@
+package net._4programmers._4note.dao.impl;
+
+import net._4programmers._4note.dao.UserDao;
+import net._4programmers._4note.entities.User;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public User getUserById(Integer id) {
+        return em.find(User.class, id);
+    }
+
+    @Transactional
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Transactional
+    public void deleteUser(User user) {
+        em.remove(user);
+    }
+
+}
