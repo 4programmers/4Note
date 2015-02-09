e267172788f046e0f9ae9d6a97cb195bc2c7f924
Andrzej Sołtysik <svizz17@gmail.com>
2015-02-09 19:37:37 +0100

    Issue #3 closed, views and database config working

--- src/main/java/net/_4programmers/_4note/controllers/UsersController.java
+++ src/main/java/net/_4programmers/_4note/controllers/UsersController.java
@@ -3,7 +3,6 @@ package net._4programmers._4note.controllers;
 import net._4programmers._4note.dao.UserDao;
 import net._4programmers._4note.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +15,17 @@ public class UsersController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping("/user/{userId}")
-    public String getUserById(@PathVariable int userId, Model model){
+    @RequestMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+
+    @RequestMapping("/users/{userId}")
+    public String getUser(@PathVariable int userId, Model model){
+        User user = userDao.getUserById(userId);
+        if(user == null)
+            return "redirect:/";
         model.addAttribute("user", userDao.getUserById(userId));
         return "user";
-    }
-
-    @RequestMapping("/") public String index(){
-        return "index";
-    }
-
-    @RequestMapping("/index") public String index2(){
-        return "index";
     }
 }
